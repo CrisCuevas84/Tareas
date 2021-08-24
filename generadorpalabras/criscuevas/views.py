0dc805e4d41a331b3d4d330c26def1a08d3cbcca
CrisCuevas84 <ccuevas84@ug.uchile.cl>
2021-08-24 19:16:36 -0400

    no recuerdo los cambios

--- generadorpalabras/criscuevas/views.py
+++ generadorpalabras/criscuevas/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, HttpResponse
 
+from .models import Jugador
+
 def index(request):
     """ return HttpResponse("app de Toshi") """
     return render(request, "criscuevas/index.html")
 
 
 def crear(request):
-    return render(request, 'criscuevas/crear.html')    
+    # request.post['parametro']
+    # capturando los parametros enviados por el formulario
+    Jugador.objects.create(
+    player_name = request.POST['player_name'],
+    player_number = request.POST['player_number'],
+    player_team = request.POST['player_team']
+    )
+    return render(request, "criscuevas/crear.html")
