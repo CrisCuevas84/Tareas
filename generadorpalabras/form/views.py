ece8da1148a5c1e6a9c798de23897070bd3a0295
CrisCuevas84 <ccuevas84@ug.uchile.cl>
2021-08-25 01:23:27 -0400

    Creamos variables para obtener los datos de manera individual

--- generadorpalabras/form/views.py
+++ generadorpalabras/form/views.py
@@ -6,6 +6,14 @@ def index(request):
 
 
 def create_user(request):
+    # Imprime mensaje en terminal
     print("Got Post Info....................")
+    # Imprime datos en un diccionario en el terminal
     print(request.POST)
+
+    # Creamos variables para obtener los datos de manera individual
+    name_from_form = request.POST['name']
+    email_from_form = request.POST['email']
+    print(name_from_form)
+    print(email_from_form)
     return render(request,"form/index.html")
