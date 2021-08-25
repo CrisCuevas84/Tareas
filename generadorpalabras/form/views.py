61d00712f0f0424c875ef2d5f4d69cf80af1b404
CrisCuevas84 <ccuevas84@ug.uchile.cl>
2021-08-25 01:10:35 -0400

    agregando metodo create user

--- generadorpalabras/form/views.py
+++ generadorpalabras/form/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, "form/index.html")
+
+
+def create_user(request):
+    print("Got Post Info....................")
+    print(request.POST)
+    return render(request,"form/index.html")
