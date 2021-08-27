b0b8360e8ed77979a2d85f863541d20c6a45f800
CrisCuevas84 <ccuevas84@ug.uchile.cl>
2021-08-27 15:00:54 -0400

    agregando palabra aleatoria

--- wordgenerator/random_word/views.py
+++ wordgenerator/random_word/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.utils.crypto import get_random_string
 
 
 def index(request):
-    return HttpResponse("prueba de que la app funciona")
+    unique_id = get_random_string(length=14)
+
+    context = {
+            'unique_id':unique_id,
+    } 
+
+    return render(request, 'index.html', context)
+
