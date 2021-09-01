63c1c34fbfe1a521ada5a62808c3e69adc4f15fd
CrisCuevas84 <ccuevas84@ug.uchile.cl>
2021-09-01 17:45:59 -0400

    agregando estilo

--- wordgenerator/random_word/views.py
+++ wordgenerator/random_word/views.py
@@ -3,11 +3,14 @@ from django.utils.crypto import get_random_string
 
 
 def index(request):
+    request.session['intentos'] = 0
     unique_id = get_random_string(length=14)
-
     context = {
             'unique_id':unique_id,
     } 
-
     return render(request, 'index.html', context)
+
+def generar(request):
+	request.session['intentos'] = 0
+	return render(request, 'index.html')
 
